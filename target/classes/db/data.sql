2bee5839d90ab46f77437528a6fda0fa772a6ad5
levycandido <31323642+levycandido@users.noreply.github.com>
2024-07-13 08:45:48 -0300

    Adding git ignore file

--- target/classes/db/data.sql
+++
@@ -1,8 +0,0 @@
--- Insert initial data into the `place` table
-
-INSERT INTO "url" (original_url, short_url, expiry_date, last_access_time)
-VALUES ('https://example.com/1', 'abc123', '2024-12-31 23:59:59', '2024-12-31 22:59:59'),
-       ('https://example.com/2', 'def456', '2024-12-31 23:59:59', '2024-12-31 22:59:59'),
-       ('https://example.com/3', 'ghi789', '2024-12-31 23:59:59', '2024-12-31 22:59:59');
-
-
