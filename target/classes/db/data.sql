0883982c7d82d0ab7a8fb0edab25a4d0f5149361
levycandido <31323642+levycandido@users.noreply.github.com>
2024-07-13 08:42:03 -0300

    Changing the project name

---
+++ target/classes/db/data.sql
@@ -0,0 +1,8 @@
+-- Insert initial data into the `place` table
+
+INSERT INTO "url" (original_url, short_url, expiry_date, last_access_time)
+VALUES ('https://example.com/1', 'abc123', '2024-12-31 23:59:59', '2024-12-31 22:59:59'),
+       ('https://example.com/2', 'def456', '2024-12-31 23:59:59', '2024-12-31 22:59:59'),
+       ('https://example.com/3', 'ghi789', '2024-12-31 23:59:59', '2024-12-31 22:59:59');
+
+
